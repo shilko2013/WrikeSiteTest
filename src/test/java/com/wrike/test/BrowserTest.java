9a0f2b6bca64a22e86b573ca0fc88bba1557ff8e
shilko2013 <shilko2013@mail.ru>
2019-07-30 11:49:42 +0300

    Assert redirect added.

--- src/test/java/com/wrike/test/BrowserTest.java
+++ src/test/java/com/wrike/test/BrowserTest.java
@@ -4,14 +4,19 @@ import com.wrike.test.config.WebDriverConfig;
 import com.wrike.test.pages.MainPage;
 import com.wrike.test.steps.MainPageSteps;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BrowserTest {
 
     protected WebDriver webDriver;
     private MainPageSteps mainPageSteps;
+    private static String redirectedTitle = "Thank you for choosing Wrike!";
+    private static long redirectedWaitTimeSeconds = 10;
 
     @Before
     public void setUp() {
@@ -22,6 +27,9 @@ public abstract class BrowserTest {
     public void test() {
         mainPageSteps = new MainPageSteps(webDriver);
         mainPageSteps.startForFree();
+        new WebDriverWait(webDriver, redirectedWaitTimeSeconds)
+                .until(ExpectedConditions.titleIs(redirectedTitle));
+        Assert.assertEquals(redirectedTitle, webDriver.getTitle());
     }
 
     @After
