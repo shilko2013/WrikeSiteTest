d61af480f3216d5a89fd2c0a16371fe5adb8ad95
shilko2013 <shilko2013@mail.ru>
2019-07-31 14:24:58 +0300

    History copy.

--- src/test/java/com/wrike/test/BrowserTest.java
+++ src/test/java/com/wrike/test/BrowserTest.java
@@ -4,7 +4,7 @@ import com.wrike.test.config.WebDriverConfig;
 import com.wrike.test.steps.MainPageSteps;
 import com.wrike.test.steps.VerifyEmailPageSteps;
 import com.wrike.test.util.RandomTextStringGenerator;
-import io.qameta.allure.Description;
+import io.qameta.allure.*;
 import io.qameta.allure.junit4.DisplayName;
 import javafx.util.Pair;
 import org.junit.*;
@@ -29,6 +29,9 @@ public abstract class BrowserTest {
     @Test
     @DisplayName("Start for free email validation")
     @Description("Trying to validate email. Filling Q&A section.")
+    @Epic("Frontend")
+    @Features({@Feature("Main page"), @Feature("Verify page")})
+    @Story("Start for free with email address")
     public void testVerifyEmail() {
         testMainPage();
         testVerifyEmailPage();
@@ -50,6 +53,9 @@ public abstract class BrowserTest {
     @Test
     @DisplayName("Footer twitter button validation")
     @Description("Checking twitter redirect URL. Checking twitter icon.")
+    @Epic("Frontend")
+    @Feature("Main page")
+    @Story("Twitter icon")
     public void testTwitterIcon() {
         MainPageSteps mainPageSteps = new MainPageSteps(webDriver);
         Assert.assertEquals(mainPageSteps.getTwitterReference(),twitterReference);
